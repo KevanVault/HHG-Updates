838ff72ab5656145bb25f7891af28e4a436f8993
KevanVault <kevinmarrero2002@gmail.com>
2026-09-10 00:00:14 +0100

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -14,7 +14,7 @@ FPS = 60
 BG_COLOR = (30, 30, 30)
 TEXT_COLOR = (255, 255, 255)
 VERSION_LOCAL = "1.3"
-# actualizado
+#actualizado
 class Button:
     def __init__(self, text, x, y, width, height, color, callback):
         self.rect = pygame.Rect(x, y, width, height)
